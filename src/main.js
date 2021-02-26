9e31f8c70205a67a5153ef039ef7e9423b4e44de
Olumide <volumide42@gmail.com>
2021-02-26 11:16:38 +0100

    configuring sass

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
